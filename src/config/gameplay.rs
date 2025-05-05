e32277d4a4a9d4c99f051c1d9b4782c7f0d0265f
MetroMan <services@metroman.me>
2025-05-05 15:10:53 -0400

    Update gameplay.rs

--- src/config/gameplay.rs
+++ src/config/gameplay.rs
@@ -1,7 +1,7 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Clone, Serialize, Deserialize)]
-pub struct EngineConfig {
+pub struct GameplayConfig {
 
     // Gameplay
     pub save_interval: f32,
