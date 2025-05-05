1c70541afd56fcecb274f394d5ff6c766acc840d
MetroMan <services@metroman.me>
2025-05-05 15:05:18 -0400

    Update core.rs

--- src/config/core.rs
+++ src/config/core.rs
@@ -1,16 +1,5 @@
-// core.rs
 pub mod rendering;
 pub mod worldgen;
 
 pub use rendering::EngineConfig;
 pub use worldgen::WorldGenConfig;
-
-/// Common derive macro for configuration types
-macro_rules! config_derive {
-    ($($t:ty),+) => {
-        $(
-            #[derive(Clone, Serialize, Deserialize)]
-            pub struct $t;
-        )+
-    };
-}
