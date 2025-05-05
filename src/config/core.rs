ab3d2c47a7cbc26c19818794707d24efe9945e37
MetroMan <services@metroman.me>
2025-05-05 15:09:21 -0400

    Update core.rs

--- src/config/core.rs
+++ src/config/core.rs
@@ -1,7 +1,10 @@
 pub mod rendering;
 pub mod worldgen;
 pub mod chunksys;
+pub mod gameplay;
 
 pub use rendering::EngineConfig;
 pub use worldgen::WorldGenConfig;
 pub use chunksys::ChunkSysConfig;
+pub use gameplay::GameplayConfig;
+ 
