1dd47ffb07a8688b0d89ac20fbd47e22a79e7a46
chojuninengu <chojuninengu@gmail.com>
2025-05-06 22:42:57 +0100

    Refactor BLOCKS initialization using lazy_static for improved runtime flexibility
    - Converted the BLOCKS constant to a lazy_static variable, allowing for dynamic initialization of block properties at runtime.
    - Updated block definitions for stone, grass, water, lava, sand, and glass to include texture faces, variations, and physics properties, enhancing the overall structure and maintainability of block data.

--- src/world/blocks_data.rs
+++ src/world/blocks_data.rs
@@ -6,6 +6,7 @@ use super::block_id::{BlockCategory, BlockDefinition, BlockId, BlockVariant, Col
 use super::block_material::{BlockMaterial, MaterialModifiers, TintSettings};
 use super::block_orientation::BlockOrientation;
 use crate::world::block_tech::{BlockFlags as TechBlockFlags, BlockPhysics};
+use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, HashSet};
 
@@ -180,163 +181,182 @@ impl Default for BlockRegistry {
     }
 }
 
-pub const BLOCKS: &[BlockDefinition] = &[
-    // 1: Stone
-    BlockDefinition {
-        id: BlockId::new(1, 0, 0),
-        name: String::new(), // Will be initialized at runtime
-        category: BlockCategory::Solid,
-        default_facing: BlockFacing::None,
-        default_orientation: BlockOrientation::None,
-        connects_to: HashSet::new(),   // Will be initialized at runtime
-        texture_faces: HashMap::new(), // Will be initialized at runtime
-        material: BlockMaterial::new([0.8, 0.8, 0.8, 1.0], 0.7, 0.0, 0.0),
-        flags: TechBlockFlags::SOLID,
-        variations: Vec::new(),       // Will be initialized at runtime
-        color_variations: Vec::new(), // Will be initialized at runtime
-        tint_settings: TintSettings {
-            strength: 0.5,
-            preserve_metallic: true,
-            preserve_roughness: true,
+lazy_static! {
+    pub static ref BLOCKS: Vec<BlockDefinition> = vec![
+        // 1: Stone
+        BlockDefinition {
+            id: BlockId::new(1, 0, 0),
+            name: "stone".into(),
+            category: BlockCategory::Solid,
+            default_facing: BlockFacing::None,
+            default_orientation: BlockOrientation::None,
+            connects_to: HashSet::new(),
+            texture_faces: HashMap::from([
+                (BlockFacing::PosZ, "stone_side.png".into()),
+                (BlockFacing::NegZ, "stone_side.png".into()),
+                (BlockFacing::PosX, "stone_side.png".into()),
+                (BlockFacing::NegX, "stone_side.png".into()),
+                (BlockFacing::PosY, "stone_top.png".into()),
+                (BlockFacing::NegY, "stone_bottom.png".into()),
+            ]),
+            material: BlockMaterial::new([0.8, 0.8, 0.8, 1.0], 0.7, 0.0, 0.0),
+            flags: TechBlockFlags::SOLID,
+            variations: vec![BlockVariant {
+                id: 1,
+                name: "cracked".into(),
+                texture_overrides: HashMap::from([(BlockFacing::None, "stone_cracked.png".into())]),
+                material_modifiers: MaterialModifiers::default(),
+            }],
+            color_variations: vec![ColorVariant {
+                id: 1,
+                name: "mossy".into(),
+                color: [0.4, 0.5, 0.3, 1.0],
+                material_modifiers: MaterialModifiers::default(),
+            }],
+            tint_settings: TintSettings {
+                strength: 0.5,
+                preserve_metallic: true,
+                preserve_roughness: true,
+            },
+            physics: BlockPhysics::solid(),
         },
-        physics: BlockPhysics::solid(),
-    },
-    // 2: Grass
-    BlockDefinition {
-        id: BlockId::new(2, 0, 0),
-        name: "grass".into(),
-        category: crate::world::block_id::BlockCategory::Solid,
-        default_facing: BlockFacing::None,
-        default_orientation: BlockOrientation::None,
-        connects_to: HashSet::new(),
-        texture_faces: HashMap::from([
-            (BlockFacing::PosZ, "grass_side.png".into()),
-            (BlockFacing::NegZ, "grass_side.png".into()),
-            (BlockFacing::PosX, "grass_side.png".into()),
-            (BlockFacing::NegX, "grass_side.png".into()),
-            (BlockFacing::PosY, "grass_top.png".into()),
-            (BlockFacing::NegY, "dirt.png".into()),
-        ]),
-        material: BlockMaterial::new([0.4, 0.8, 0.3, 1.0], 0.9, 0.0, 0.0),
-        flags: TechBlockFlags::SOLID,
-        variations: vec![],
-        color_variations: vec![],
-        tint_settings: TintSettings {
-            strength: 0.5,
-            preserve_metallic: true,
-            preserve_roughness: true,
+        // 2: Grass
+        BlockDefinition {
+            id: BlockId::new(2, 0, 0),
+            name: "grass".into(),
+            category: crate::world::block_id::BlockCategory::Solid,
+            default_facing: BlockFacing::None,
+            default_orientation: BlockOrientation::None,
+            connects_to: HashSet::new(),
+            texture_faces: HashMap::from([
+                (BlockFacing::PosZ, "grass_side.png".into()),
+                (BlockFacing::NegZ, "grass_side.png".into()),
+                (BlockFacing::PosX, "grass_side.png".into()),
+                (BlockFacing::NegX, "grass_side.png".into()),
+                (BlockFacing::PosY, "grass_top.png".into()),
+                (BlockFacing::NegY, "dirt.png".into()),
+            ]),
+            material: BlockMaterial::new([0.4, 0.8, 0.3, 1.0], 0.9, 0.0, 0.0),
+            flags: TechBlockFlags::SOLID,
+            variations: vec![],
+            color_variations: vec![],
+            tint_settings: TintSettings {
+                strength: 0.5,
+                preserve_metallic: true,
+                preserve_roughness: true,
+            },
+            physics: BlockPhysics::solid(),
         },
-        physics: BlockPhysics::solid(),
-    },
-    // 3: Water
-    BlockDefinition {
-        id: BlockId::new(3, 0, 0),
-        name: "water".into(),
-        category: crate::world::block_id::BlockCategory::Liquid,
-        default_facing: BlockFacing::None,
-        default_orientation: BlockOrientation::None,
-        connects_to: HashSet::from([crate::world::block_id::BlockCategory::Liquid]),
-        texture_faces: HashMap::from([
-            (BlockFacing::PosZ, "water.png".into()),
-            (BlockFacing::NegZ, "water.png".into()),
-            (BlockFacing::PosX, "water.png".into()),
-            (BlockFacing::NegX, "water.png".into()),
-            (BlockFacing::PosY, "water.png".into()),
-            (BlockFacing::NegY, "water.png".into()),
-        ]),
-        material: BlockMaterial::new([0.2, 0.4, 0.8, 0.8], 0.1, 0.0, 0.0),
-        flags: TechBlockFlags::LIQUID,
-        variations: vec![],
-        color_variations: vec![],
-        tint_settings: TintSettings {
-            strength: 0.5,
-            preserve_metallic: true,
-            preserve_roughness: true,
+        // 3: Water
+        BlockDefinition {
+            id: BlockId::new(3, 0, 0),
+            name: "water".into(),
+            category: crate::world::block_id::BlockCategory::Liquid,
+            default_facing: BlockFacing::None,
+            default_orientation: BlockOrientation::None,
+            connects_to: HashSet::from([crate::world::block_id::BlockCategory::Liquid]),
+            texture_faces: HashMap::from([
+                (BlockFacing::PosZ, "water.png".into()),
+                (BlockFacing::NegZ, "water.png".into()),
+                (BlockFacing::PosX, "water.png".into()),
+                (BlockFacing::NegX, "water.png".into()),
+                (BlockFacing::PosY, "water.png".into()),
+                (BlockFacing::NegY, "water.png".into()),
+            ]),
+            material: BlockMaterial::new([0.2, 0.4, 0.8, 0.8], 0.1, 0.0, 0.0),
+            flags: TechBlockFlags::LIQUID,
+            variations: vec![],
+            color_variations: vec![],
+            tint_settings: TintSettings {
+                strength: 0.5,
+                preserve_metallic: true,
+                preserve_roughness: true,
+            },
+            physics: BlockPhysics::liquid(),
         },
-        physics: BlockPhysics::liquid(),
-    },
-    // 4: Lava
-    BlockDefinition {
-        id: BlockId::new(4, 0, 0),
-        name: "lava".into(),
-        category: crate::world::block_id::BlockCategory::Liquid,
-        default_facing: BlockFacing::None,
-        default_orientation: BlockOrientation::None,
-        connects_to: HashSet::from([crate::world::block_id::BlockCategory::Liquid]),
-        texture_faces: HashMap::from([
-            (BlockFacing::PosZ, "lava.png".into()),
-            (BlockFacing::NegZ, "lava.png".into()),
-            (BlockFacing::PosX, "lava.png".into()),
-            (BlockFacing::NegX, "lava.png".into()),
-            (BlockFacing::PosY, "lava.png".into()),
-            (BlockFacing::NegY, "lava.png".into()),
-        ]),
-        material: BlockMaterial::new([0.8, 0.2, 0.1, 0.8], 0.1, 0.0, 0.5),
-        flags: TechBlockFlags::LIQUID,
-        variations: vec![],
-        color_variations: vec![],
-        tint_settings: TintSettings {
-            strength: 0.5,
-            preserve_metallic: true,
-            preserve_roughness: true,
+        // 4: Lava
+        BlockDefinition {
+            id: BlockId::new(4, 0, 0),
+            name: "lava".into(),
+            category: crate::world::block_id::BlockCategory::Liquid,
+            default_facing: BlockFacing::None,
+            default_orientation: BlockOrientation::None,
+            connects_to: HashSet::from([crate::world::block_id::BlockCategory::Liquid]),
+            texture_faces: HashMap::from([
+                (BlockFacing::PosZ, "lava.png".into()),
+                (BlockFacing::NegZ, "lava.png".into()),
+                (BlockFacing::PosX, "lava.png".into()),
+                (BlockFacing::NegX, "lava.png".into()),
+                (BlockFacing::PosY, "lava.png".into()),
+                (BlockFacing::NegY, "lava.png".into()),
+            ]),
+            material: BlockMaterial::new([0.8, 0.2, 0.1, 0.8], 0.1, 0.0, 0.5),
+            flags: TechBlockFlags::LIQUID,
+            variations: vec![],
+            color_variations: vec![],
+            tint_settings: TintSettings {
+                strength: 0.5,
+                preserve_metallic: true,
+                preserve_roughness: true,
+            },
+            physics: BlockPhysics::liquid(),
         },
-        physics: BlockPhysics::liquid(),
-    },
-    // 5: Sand
-    BlockDefinition {
-        id: BlockId::new(5, 0, 0),
-        name: "sand".into(),
-        category: crate::world::block_id::BlockCategory::Solid,
-        default_facing: BlockFacing::None,
-        default_orientation: BlockOrientation::None,
-        connects_to: HashSet::new(),
-        texture_faces: HashMap::from([
-            (BlockFacing::PosZ, "sand.png".into()),
-            (BlockFacing::NegZ, "sand.png".into()),
-            (BlockFacing::PosX, "sand.png".into()),
-            (BlockFacing::NegX, "sand.png".into()),
-            (BlockFacing::PosY, "sand.png".into()),
-            (BlockFacing::NegY, "sand.png".into()),
-        ]),
-        material: BlockMaterial::new([0.9, 0.9, 0.7, 1.0], 0.9, 0.0, 0.0),
-        flags: TechBlockFlags::SOLID,
-        variations: vec![],
-        color_variations: vec![],
-        tint_settings: TintSettings {
-            strength: 0.5,
-            preserve_metallic: true,
-            preserve_roughness: true,
+        // 5: Sand
+        BlockDefinition {
+            id: BlockId::new(5, 0, 0),
+            name: "sand".into(),
+            category: crate::world::block_id::BlockCategory::Solid,
+            default_facing: BlockFacing::None,
+            default_orientation: BlockOrientation::None,
+            connects_to: HashSet::new(),
+            texture_faces: HashMap::from([
+                (BlockFacing::PosZ, "sand.png".into()),
+                (BlockFacing::NegZ, "sand.png".into()),
+                (BlockFacing::PosX, "sand.png".into()),
+                (BlockFacing::NegX, "sand.png".into()),
+                (BlockFacing::PosY, "sand.png".into()),
+                (BlockFacing::NegY, "sand.png".into()),
+            ]),
+            material: BlockMaterial::new([0.9, 0.9, 0.7, 1.0], 0.9, 0.0, 0.0),
+            flags: TechBlockFlags::SOLID,
+            variations: vec![],
+            color_variations: vec![],
+            tint_settings: TintSettings {
+                strength: 0.5,
+                preserve_metallic: true,
+                preserve_roughness: true,
+            },
+            physics: BlockPhysics::solid(),
         },
-        physics: BlockPhysics::solid(),
-    },
-    // 6: Glass
-    BlockDefinition {
-        id: BlockId::new(6, 0, 0),
-        name: "glass".into(),
-        category: crate::world::block_id::BlockCategory::Transparent,
-        default_facing: BlockFacing::None,
-        default_orientation: BlockOrientation::None,
-        connects_to: HashSet::new(),
-        texture_faces: HashMap::from([
-            (BlockFacing::PosZ, "glass.png".into()),
-            (BlockFacing::NegZ, "glass.png".into()),
-            (BlockFacing::PosX, "glass.png".into()),
-            (BlockFacing::NegX, "glass.png".into()),
-            (BlockFacing::PosY, "glass.png".into()),
-            (BlockFacing::NegY, "glass.png".into()),
-        ]),
-        material: BlockMaterial::new([0.9, 0.9, 0.9, 0.3], 0.1, 0.0, 0.0),
-        flags: TechBlockFlags::SOLID,
-        variations: vec![],
-        color_variations: vec![],
-        tint_settings: TintSettings {
-            strength: 0.5,
-            preserve_metallic: true,
-            preserve_roughness: true,
+        // 6: Glass
+        BlockDefinition {
+            id: BlockId::new(6, 0, 0),
+            name: "glass".into(),
+            category: crate::world::block_id::BlockCategory::Transparent,
+            default_facing: BlockFacing::None,
+            default_orientation: BlockOrientation::None,
+            connects_to: HashSet::new(),
+            texture_faces: HashMap::from([
+                (BlockFacing::PosZ, "glass.png".into()),
+                (BlockFacing::NegZ, "glass.png".into()),
+                (BlockFacing::PosX, "glass.png".into()),
+                (BlockFacing::NegX, "glass.png".into()),
+                (BlockFacing::PosY, "glass.png".into()),
+                (BlockFacing::NegY, "glass.png".into()),
+            ]),
+            material: BlockMaterial::new([0.9, 0.9, 0.9, 0.3], 0.1, 0.0, 0.0),
+            flags: TechBlockFlags::SOLID,
+            variations: vec![],
+            color_variations: vec![],
+            tint_settings: TintSettings {
+                strength: 0.5,
+                preserve_metallic: true,
+                preserve_roughness: true,
+            },
+            physics: BlockPhysics::solid(),
         },
-        physics: BlockPhysics::solid(),
-    },
-];
+    ];
+}
 
 pub fn create_default_block() -> (TechBlockFlags, BlockMaterial) {
     let flags = TechBlockFlags::NONE;
