762fef402ccda17bbcbea7ab6d981a6fe8b7ce56
chojuninengu <chojuninengu@gmail.com>
2025-05-06 19:18:03 +0100

    Add ChunkCoord struct with constructor for 3D coordinates using IVec3, enhancing spatial representation in the world module.

--- src/world/chunk_coord.rs
+++ src/world/chunk_coord.rs
@@ -1,1 +1,10 @@
+use glam::IVec3;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct ChunkCoord(pub IVec3);
+
+impl ChunkCoord {
+    pub fn new(x: i32, y: i32, z: i32) -> Self {
+        Self(IVec3::new(x, y, z))
+    }
+}
