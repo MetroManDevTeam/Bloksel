bc3f5ed8d8b29695d33e5211eb48e593eabc8be7
chojuninengu <chojuninengu@gmail.com>
2025-05-06 16:17:39 +0100

    Refactor Block struct to include orientation and facing properties, updating the id type to u16. Introduced methods for setting orientation and facing, enhancing the flexibility and clarity of block management.

--- src/world/block.rs
+++ src/world/block.rs
@@ -1,11 +1,13 @@
+use crate::world::block_facing::BlockFacing as BlockFacingImport;
 use crate::world::{BlockFacing, BlockId, BlockOrientation, ConnectedDirections};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Block {
-    pub id: BlockId,
-    pub sub_blocks: HashMap<(u8, u8, u8), SubBlock>,
-    pub resolution: u8,
+    pub id: u16,
+    pub orientation: BlockOrientation,
+    pub facing: BlockFacingImport,
 }
 
 #[derive(Debug, Clone)]
@@ -18,12 +20,22 @@ pub struct SubBlock {
 }
 
 impl Block {
-    pub fn new(id: BlockId, resolution: u8) -> Self {
+    pub fn new(id: u16) -> Self {
         Self {
             id,
-            sub_blocks: HashMap::new(),
-            resolution,
+            orientation: BlockOrientation::None,
+            facing: BlockFacing::None,
         }
+    }
+
+    pub fn with_orientation(mut self, orientation: BlockOrientation) -> Self {
+        self.orientation = orientation;
+        self
+    }
+
+    pub fn with_facing(mut self, facing: BlockFacing) -> Self {
+        self.facing = facing;
+        self
     }
 
     pub fn place_sub_block(&mut self, x: u8, y: u8, z: u8, sub_block: SubBlock) {
