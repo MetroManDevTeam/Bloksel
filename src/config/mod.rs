1ea3ee9e1025b0539228057a6a1a67c2390925b5
chojuninengu <chojuninengu@gmail.com>
2025-05-06 13:23:10 +0100

    Add mod.rs to define configuration modules for chunks, core, gameplay, rendering, and world generation

---
+++ src/config/mod.rs
@@ -0,0 +1,11 @@
+pub mod chunksys;
+pub mod core;
+pub mod game;
+pub mod gameplay;
+pub mod rendering;
+pub mod worldgen;
+
+pub use chunksys::ChunkSysConfig;
+pub use core::EngineConfig;
+pub use gameplay::GameplayConfig;
+pub use worldgen::WorldGenConfig;
