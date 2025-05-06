b204aa99b83737f5328eb12ecd7101db3d9056cc
chojuninengu <chojuninengu@gmail.com>
2025-05-06 19:52:40 +0100

    Refactor imports in terrain.rs to include BlockFacing and remove redundant import of BlockFacing from block_visual, improving code organization and clarity.

--- src/world/generator/terrain.rs
+++ src/world/generator/terrain.rs
@@ -1,7 +1,8 @@
 use crate::world::BlockOrientation;
 use crate::world::block::{Block, SubBlock};
+use crate::world::block_facing::BlockFacing;
 use crate::world::block_id::BlockId;
-use crate::world::block_visual::{BlockFacing, ConnectedDirections};
+use crate::world::block_visual::ConnectedDirections;
 use crate::world::blocks_data::BlockRegistry;
 use crate::world::chunk::Chunk;
 use crate::world::chunk_coord::ChunkCoord;
