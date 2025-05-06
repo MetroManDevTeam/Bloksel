171ee1943282342c04bc300097b630f9b6bec004
chojuninengu <chojuninengu@gmail.com>
2025-05-06 13:34:00 +0100

    Add WorldGenConfig struct in worldgen.rs to include new fields for structure generation, cave generation, ore generation, tree generation, tree density, ore density, and structure frequency, enhancing world generation configuration options.

--- src/config/worldgen.rs
+++ src/config/worldgen.rs
@@ -1,7 +1,14 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct WorldGenConfig {
     // World Generation
     pub world_seed: u64,
+    pub generate_structures: bool,
+    pub generate_caves: bool,
+    pub generate_ores: bool,
+    pub generate_trees: bool,
+    pub tree_density: f32,
+    pub ore_density: f32,
+    pub structure_frequency: f32,
 }
