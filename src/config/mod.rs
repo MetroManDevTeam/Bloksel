79162012a944f123f1136fc4178fc0d93c7e9a39
chojuninengu <chojuninengu@gmail.com>
2025-05-07 17:03:22 +0100

    refactor: update configuration exports to remove TerrainConfig and streamline gameplay imports

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -7,6 +7,6 @@ pub mod worldgen;
 
 pub use chunksys::ChunkSysConfig;
 pub use core::EngineConfig;
-pub use game::{TerrainConfig, GameplayConfig, RenderConfig};
-pub use rendering::RenderConfig as RenderingConfig;
+pub use gameplay::GameplayConfig;
+pub use rendering::RenderConfig;
 pub use worldgen::WorldGenConfig;
