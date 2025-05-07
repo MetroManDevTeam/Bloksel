6b5f929d5a631414101cea33b691db65b8a50523
MetroMan <services@metroman.me>
2025-05-07 18:59:52 -0400

    Added Texture Export to core.rs

--- src/render/core.rs
+++ src/render/core.rs
@@ -2,9 +2,11 @@ pub mod camera;
 pub mod mesh;
 pub mod pipeline;
 pub mod shaders;
+pub mod texture
 
 // Remove duplicate imports and re-exports
 pub use camera::Camera;
 pub use mesh::{Mesh, MeshData};
 pub use pipeline::RenderPipeline;
 pub use shaders::Shader;
+pub use texture::Texture;
