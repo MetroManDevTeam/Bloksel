6255dc8c2c77f0782dba4a686a6016c0d5f82eae
chojuninengu <chojuninengu@gmail.com>
2025-05-07 17:01:27 +0100

    refactor: update configuration exports to include TerrainConfig and RenderConfig for improved modularity

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -7,5 +7,6 @@ pub mod worldgen;
 
 pub use chunksys::ChunkSysConfig;
 pub use core::EngineConfig;
-pub use gameplay::GameplayConfig;
+pub use game::{TerrainConfig, GameplayConfig, RenderConfig};
+pub use rendering::RenderConfig as RenderingConfig;
 pub use worldgen::WorldGenConfig;
